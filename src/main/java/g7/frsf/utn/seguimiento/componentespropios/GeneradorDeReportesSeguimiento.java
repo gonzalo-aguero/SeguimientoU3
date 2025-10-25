8c050d9184732e08990db20da2d19bec79179220
Gonzalo Aguero <gonzaloaguerodev@gmail.com>
2025-10-25 19:04:42 -0300

    ads

--- src/main/java/g7/frsf/utn/seguimiento/componentespropios/GeneradorDeReportesSeguimiento.java
+++ src/main/java/g7/frsf/utn/seguimiento/componentespropios/GeneradorDeReportesSeguimiento.java
@@ -11,22 +11,22 @@ public class GeneradorDeReportesSeguimiento extends GeneradorDeReportes {
         System.out.println("-----------------------------------------------------------");
         System.out.println("**********REPORTE DE SIMULACION DE SEGUIMIENTO U3**********");
         System.out.println("-----------------------------------------------------------");
-        System.out.println("Cantidad de clientes Procesados: " + contadoresSeguimiento.getClientesProcesados());
-        System.out.println("Beneficio Total Acumulado: " + contadoresSeguimiento.getBeneficioTotalAcumulado());
+        System.out.println("Cantidad de Clientes Atendidos: " + contadoresSeguimiento.getClientesProcesados());
+        System.out.println("Beneficio Total Acumulado: " + String.format("%.2f", contadoresSeguimiento.getBeneficioTotalAcumulado()) + " unidades monetarias");
 
         // Calculo de la longitud promedio de la cola
         Double longitudPromedioCola = contadoresSeguimiento.getLongitudColaAcumulada().doubleValue() / Main.getTiempoASimular();
-        System.out.println("Longitud Promedio de la Cola: " + longitudPromedioCola);
+        System.out.println("Longitud Promedio de la Cola: " + String.format("%.3f", longitudPromedioCola) + " clientes");
 
         // Calculo del tiempo promedio de los clientes en el kiosco
         Double tiempoPromedioClientesEnKiosco = (contadoresSeguimiento.getTiempoClientesEnKioscoAcumulado() / contadoresSeguimiento.getClientesProcesados()) / 60; // Convertir a horas
         Double minutosPromedioClientesEnKiosco = tiempoPromedioClientesEnKiosco * 60;
         System.out.println("Tiempo Promedio de Clientes en Kiosco: " + String.format("%.3f", tiempoPromedioClientesEnKiosco) + " horas (" + String.format("%.3f", minutosPromedioClientesEnKiosco) + " minutos)");
-
+ 
         // Calculo de la tasa de atencion por empleada
         Double tasaAtencionPorEmpleada = contadoresSeguimiento.getClientesProcesados().doubleValue() / Main.getTiempoASimular();
         Double tasaAtencionPorHora = tasaAtencionPorEmpleada * 60;
-        System.out.println("Tasa de Atención por Empleada: " + tasaAtencionPorHora + " clientes por hora");
+        System.out.println("Tasa de Atención por Empleada: " + String.format("%.3f", tasaAtencionPorHora) + " clientes por hora");
 
         // Calculo del porcentaje de tiempo libre de la empleada
         Double tiempoTotalSimulacion = Double.valueOf(Main.getTiempoASimular());
